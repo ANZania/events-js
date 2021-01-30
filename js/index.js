e81325f7bf6790815750245976dfe2105b82834f
ANZania <zianastasia@yandex.ru>
2021-01-30 16:00:23 +0300

    Add event notifications, create notificationButtons style changing logic

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 'use strict'
 
 const eventsListWrapper = document.querySelector('.events-list__wrapper');
+const subscriptionButton = document.querySelector('.container__header__subscription-button');
 const requestURL = './data.json';
-
-let eventsData = [];
+let eventsData;
 
 const isFinished = date => {
     const currentDate = new Date();
@@ -20,28 +20,33 @@ const reduceTextShield = text => {
     const regExp = /[&<>"\/]/g;
 
     const specialCharMap = {
-        "&": "&amp;",
-        "<": "&lt;",
-        ">": "&gt;",
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
         '"': '&quot;',
-        "/": '&#x2F;'
+        '/': '&#x2F;'
     };
 
     text = text.replace(regExp, (char) => {
         return specialCharMap[char]
-    })
+    });
 
     return text
 }
 
-const renderNotificationButton = date => {
+const renderNotificationButton = (date, name) => {
     const status = isFinished(date);
+    let button;
 
     if (status === 'Finished') {
-        return '<button class="container__header__notify-button" disabled >Finished</button>'
+        button = '<button class="container__header__notify-button" disabled>Finished</button>'
+    } else if (localStorage.getItem(name)) {
+        button = '<button class="container__header__notify-button notify-button-disabled">Tracking...</button>'
     } else {
-        return '<button class="container__header__notify-button">Notify me!</button>'
+        button = '<button class="container__header__notify-button">Notify me!</button>'
     }
+
+    return button
 }
 
 const initializeRequest = async() => {
@@ -49,24 +54,111 @@ const initializeRequest = async() => {
     .then(response => {
         return response.json()
     })
-    .then(promise => {
-        renderEvents(promise)
+    .then(data => {
+        renderEvents(data)
     })
 }
 
-const renderEvents = (eventsData) => {
-    eventsData.forEach(element => {
+const checkIfAllTracked = (data) => {
+    const length = data.length;
+    let counter = 0;
+
+    data.forEach(element => {
+        if (localStorage.getItem(element.name) || (isFinished(element.endDate) === 'Finished')) {
+            counter++;
+        }
+    })
+
+    console.log(counter);
+    if (length === counter) {
+        localStorage.setItem('isTrackingAll', 'true');
+    }
+}
+
+const renderSubscriptionButton = (data) => {
+    checkIfAllTracked(data);
+    if (localStorage.getItem('isTrackingAll')) {
+        subscriptionButton.classList.add('notify-button-disabled');
+        subscriptionButton.textContent = 'You are tracking all events'
+    } else {
+        subscriptionButton.classList.remove('notify-button-disabled');
+        subscriptionButton.textContent = 'Notify me of all upcoming events'
+    }
+}
+
+const renderEvents = (data) => {
+    eventsData = data;
+    renderSubscriptionButton(data);
+
+    data.forEach(element => {
         eventsListWrapper.insertAdjacentHTML('beforeend', `
         <li class="events-list__element">
             <section class="events-list__element__wrapper">
                 <p class="events-list__element__text">${element.startDate}</p>
-                <a href="${element.url}" target="blank"><p class="events-list__element__text">${reduceTextShield(element.name)}</p></a>
+                <a href="${element.url}" target="blank">
+                    <p class="events-list__element__text">${reduceTextShield(element.name)}</p>
+                </a>
                 <p class="events-list__element__text">${isFinished(element.endDate)}</p>
             </section>
-            ${renderNotificationButton(element.endDate)}
+            ${renderNotificationButton(element.endDate, element.name)}
         </li>
         `)
     })
 }
 
+const initializeEventListeners = () => {
+    window.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target.closest('.container__header__notify-button')) {
+            const element = target.closest('.events-list__element');
+            const elementStartDate = element.querySelectorAll('.events-list__element__text')[0].textContent;
+            const elementName = element.querySelectorAll('.events-list__element__text')[1].textContent;
+
+            if (localStorage.getItem(elementName)) {
+                localStorage.removeItem(elementName);
+                localStorage.removeItem('isTrackingAll');
+                eventsListWrapper.textContent = '';
+                renderEvents(eventsData);
+
+                createNotification(`You have stopped tracking ${elementName}.`);
+            } else {
+                localStorage.setItem(elementName, elementStartDate);
+                eventsListWrapper.textContent = '';
+                renderEvents(eventsData);
+
+                createNotification(`You have subscribed to ${elementName} notifications. 
+                It will start on ${elementStartDate}.`);
+            }
+        }
+
+        if (target.closest('.container__header__subscription-button')) {
+            if (localStorage.getItem('isTrackingAll')) {
+                localStorage.clear();
+                eventsListWrapper.textContent = '';
+                renderEvents(eventsData);
+
+                createNotification(`You have unsubscribed from all events.`);
+
+            } else {
+                localStorage.setItem('isTrackingAll', 'true');
+                eventsListWrapper.textContent = '';
+                eventsData.forEach(element => {
+                    if (!localStorage.getItem(element.name)) {
+                        localStorage.setItem(element.name, element.startDate);
+                    }
+                })
+                renderEvents(eventsData)
+
+                createNotification(`You have subscribed to all events.`);
+            }
+
+
+        }
+
+
+    })
+}
+
 initializeRequest()
+initializeEventListeners()
